8e683a769fa6cfe5a9f68d0f98c933d96ff75adf
Reed Hunsaker <reed.hunsaker@gmail.com>
2023-01-22 01:45:42 -0600

    remove bad test

---
+++ setup_env.rb
@@ -0,0 +1,4 @@
+system("pip install bs4")
+system("pip install requests")
+system("pip install pytest")
+system("pip install -i https://test.pypi.org/simple/ sloom")
--- tests/test_env.py
+++
@@ -1,4 +0,0 @@
-from platform import python_version
-
-def test_python_directory():
-    assert python_version() == "3.10.0"
